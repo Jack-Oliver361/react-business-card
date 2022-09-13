91c758b6269691e9caedea51991a2889b750f656
Jack Oliver <Jackoliver361@gmail.com>
2022-09-13 15:07:11 +0100

    added about me component and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
-import ProfileInfo from './components/ProfileInfo'
+import './App.css';
+import AboutMe from './components/AboutMe';
+import ProfileInfo from './components/ProfileInfo';
 
 function App() {
   return (
     <div className="App">
       <ProfileInfo />
+      <AboutMe />
     </div>
   )
 }
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,15 @@
+import './AboutMe.css';
+
+function AboutMe() {
+    return (
+        <div className='about-me'>
+            <h3 className='title'>About</h3>
+            <p className='description'>
+                An enthusiastic, highly motivated person, who studied a BSc in Computing Science at Plymouth University. Dedicated worker with excellent communication,
+                time management and computer skills. Seeking to leverage my exemplary analytical skills, and coding skills.
+            </p>
+        </div>
+    )
+}
+
+export default AboutMe;
